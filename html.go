4d276e0c408f0459efd83b573b874b62cb17f5e2
gustavoespindolaoselame <gustavo.e.oselame@gmail.com>
2026-07-20 22:45:42 -0300

    yeah

--- html.go
+++ html.go
@@ -1,20 +1,37 @@
-package main
+package titletracker
 
 import (
-	"io/ioutil"
+	"io"
 	"net/http"
 	"regexp"
 )
 
-func Titulo(urls ...string) <-chan string{
+// Titulo fetches the <title> tag asynchronously for given URLs.
+// Capitalized 'T' makes it exported (public).
+func Titulo(urls ...string) <-chan string {
 	c := make(chan string)
 	for _, url := range urls {
-		go func(url string){
-			resp, _ := http.Get(url)
-			hmtl, _ := ioutil.ReadAll(resp.Body)
+		go func(url string) {
+			resp, err := http.Get(url)
+			if err != nil {
+				c <- "Error fetching URL"
+				return
+			}
+			defer resp.Body.Close()
 
-			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
-			c <- r.FindStringSubmatch(string(hmtl))[1]
+			html, err := io.ReadAll(resp.Body)
+			if err != nil {
+				c <- "Error reading body"
+				return
+			}
+
+			r, _ := regexp.Compile(`(?i)<title>(.*?)</title>`)
+			matches := r.FindStringSubmatch(string(html))
+			if len(matches) > 1 {
+				c <- matches[1]
+			} else {
+				c <- "No title found"
+			}
 		}(url)
 	}
 	return c
